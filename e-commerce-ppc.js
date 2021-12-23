76746a43a01f5e18a916bbd4c20036ae0c2c11ef
tnado <steinlagems@gmail.com>
2021-12-23 13:29:57 -0600

    update

--- e-commerce-ppc.js
+++ e-commerce-ppc.js
@@ -4,3 +4,20 @@ const fs = require("fs");
 
 const url = "https://clutch.co/agencies/ppc/ecommerce"
 
+async function scrapeData() {
+    try {
+        const { data } = await axios.get(url);
+        
+        const $ = cheerio.load(data);
+        
+        const listItems = $(".list_wrap ul li")
+
+        const companies = [];
+
+        listItems.each((idx, el) => {
+            const company = { title: "", profile_page_url: "", image_url: "", description: "" };
+
+            company.title = $(el).children("")
+        })
+    }
+}
