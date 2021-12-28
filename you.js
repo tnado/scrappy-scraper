da830c6747158e9dae8c7d90d7ac0b82a17b69a7
tnado <steinlagems@gmail.com>
2021-12-28 14:52:33 -0600

    daniel issues

---
+++ you.js
@@ -0,0 +1,46 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+const url = "https://clutch.co/agencies/ppc/ecommerce"
+
+async function scrapeData() {
+    try {
+        const { data } = await axios.get(url);
+        
+        const $ = cheerio.load(data);
+        
+        const listItems = $(".list_wrap ul li");
+
+        const companies = [];
+
+        listItems.each((idx, el) => {
+            
+            const company = { profile_page_url: "", title: "", image_url: "", description: "" };
+
+            company.profile_page_url = $(el).children("h3.company_info a").url();
+
+            company.title = $(el).children("h3.company_info a").text();
+
+            company.image_url = $(el).children("img").image_url();
+
+            company.description = $(el).children("p.company_info_wrap").text();
+
+            companies.push(company);
+        });
+
+        console.dir(companies)
+
+        fs.writeFile("companies.json"), JSON.stringify(companies, null, 2), (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log("successfully written data to file");
+        };
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+scrapeData();
